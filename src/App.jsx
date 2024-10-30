ac16f6c79fd3dcbb56c3775f04620a2c5d5815db
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-10-30 23:44:02 +0500

    added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [num, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="main">
+        <h1 className="hd">Increment Dicriment</h1>
+        <h1>{num}</h1>
+        <button onClick={()=>{setCount(num+1)}}>Plus</button>
+        <button onClick={()=>{setCount(num-1)}}>Minas</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
